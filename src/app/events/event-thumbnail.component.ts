4f96f4af5523b6780c14eda5404acc1917d1c09b
nathangiose <nathan@angleorange.com>
2022-03-22 10:55:01 +0200

    added events

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -17,8 +17,9 @@ import { Component, Input  } from '@angular/core'
     </div>
     `,
     styles: [`
+    .thumbnail {min-height:210px;}
     .pad-left {margin-left:10px};
-    .well div {color: #bbb;}
+    .well div {color: #bbb};
     `]    
     
 })
